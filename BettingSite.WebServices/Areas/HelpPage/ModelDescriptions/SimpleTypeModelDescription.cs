7eb3a3970b162a94c40136dd9d374fe7b5a7a11e
kosdim <kosdim@CB-BG-KOSDIM>
2018-03-08 13:59:22 +0200

    Add project initial structure

---
+++ BettingSite.WebServices/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BettingSite.WebServices.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ BettingSite.WebServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BettingSite.WebServices.WebApiApplication" Language="C#" %>
